f8d50fea72d5bf35f2b2cc880e829110b6f16571
Eder <ochoahudson@gmail.com>
2024-12-20 02:23:20 +0000

    primer commit

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>¡Pantalla de home!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 20,
+    color: 'black',
+  },
+});
